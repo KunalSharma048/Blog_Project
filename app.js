5fe5232c7ee0d099143f9731b4644ac83351e41b
KunalSharma048 <kunalsharma2915@gmail.com>
2022-07-28 13:11:21 +0530

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 //flash messages
 var session = require('express-session')
 var flash = require('connect-flash');
